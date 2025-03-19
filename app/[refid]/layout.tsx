702fa4ed77bfd770d74171bfb9bcebadaf90e0eb
faiza <faizanahmed0107@gmail.com>
2025-03-19 13:03:04 +0530

    changed name of site

--- app/[refid]/layout.tsx
+++ app/[refid]/layout.tsx
@@ -6,7 +6,7 @@ const Layout = (
     return (
         <>
             <div className='flex flex-col gap-1.5 items-center justify-center bg-primary p-2.5'>
-                <h1 className='text-5xl'>OmniSync</h1>
+                <h1 className='text-5xl'>ClipFlow</h1>
                 <p>A universal clipboard for all devices.</p>
             </div>
             {children}
--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "My Page",
+  title: "Clip Flow",
   description: "None",
 };
 
